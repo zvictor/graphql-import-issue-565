301f7999f37f918fabc75fcea61d4e8e8198306c
zvictor <victor0110@msn.com>
2020-04-11 14:06:39 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,34 @@
+const { importSchema } = require('graphql-import')
+
+const target = `type Post {
+  title: String!
+  content: String!
+  author: User!
+}
+
+type User {
+  username: String!
+}
+`
+
+function run(pattern) {
+  console.log('\n===========================', pattern, '===========================')
+
+  try {
+    const schema = importSchema(pattern)
+    const missing = target.replace(schema, '')
+    
+    if(missing) {
+      console.log(`\n>> FOUND <<\n${schema}`)
+      console.log(`>> MISSING <<\n${missing}`)
+    } else {
+      console.log('>> CORRECT! <<')
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+run('blog/Post.gql')
+run('**/Post.gql')
+run('**/*.gql')
